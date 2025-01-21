292c4d03cdbb1e1b4d2bb245ed3353d561355839
Ayushi Waskale <159129971+Ayushiwaskale@users.noreply.github.com>
2025-01-21 19:30:54 +0530

    Add files via upload

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const UserModel = require("../models/userModel");
+
+// login callback
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email, password });
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+//Register Callback
+const registerController = async (req, res) => {
+  console.log(req.body)
+  const {email,name,password}=req.body
+  try {
+    const newUser = new UserModel({
+      email,
+      password,
+      name
+    });
+    await newUser.save();
+    res.status(201).json({
+      success: true,
+      newUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+module.exports = { loginController, registerController };
